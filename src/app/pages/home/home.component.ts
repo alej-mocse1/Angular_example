4c89ce873141b399152a8350b98dd2a3d1f91b49
alejMocse1 <alejandromocseo1@gmail.com>
2025-02-27 17:23:25 -0300

    v 1.0.1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, HostListener } from '@angular/core';
 import { Services } from '../../serv/serv.service';
 import { Products } from '../../models/producst';
 import { CommonModule } from '@angular/common';
@@ -18,19 +18,28 @@ export class HomeComponent implements OnInit{
 
   private service = inject(Services)
   productos:Products[] = []
+  isWindowLarge = true;
   
 
   ngOnInit(): void {
+    this.checkWindowSize();
     this.service.getProducts().subscribe((resp: Array<Products>) => {
-      
       this.productos = resp.map((el) => {
         return {
           ...el,
           title: el.title.split(' ').slice(0, 3).join(' ')
-        }
-      })
-    })
+        };
+      });
+    });
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWindowSize();
+  }
+
+  checkWindowSize() {
+    this.isWindowLarge = window.innerWidth > 1000;
   }
 
 }
